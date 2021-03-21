29b7f29d7a2b41348545288d88c67c716dbc177a
mulp <fabio.cuomo@gmail.com>
2021-03-21 15:26:34 +0100

    Initial Commit

---
+++ TestDispatchWithLegacyCode/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestDispatchWithLegacyCode
+//
+//  Created by Fabio Cuomo on 21/03/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
