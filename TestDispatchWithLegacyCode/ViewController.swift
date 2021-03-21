955eb4838732ad2e3a119aef5fad217020ef2ce8
mulp <fabio.cuomo@gmail.com>
2021-03-21 15:40:23 +0100

    Adds label to the view controller

--- TestDispatchWithLegacyCode/ViewController.swift
+++ TestDispatchWithLegacyCode/ViewController.swift
@@ -8,11 +8,24 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    private let label: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 20)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
+        view.addSubview(label)
+        NSLayoutConstraint.activate([
+            label.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            label.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+        ])
+        label.text = "Loading..."
     }
+    
 
 }
 
